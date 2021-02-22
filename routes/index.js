9cab743a1d99e4e1a34e74c3eeded180bda46b99
mike <mikecee0815@gmail.com>
2021-02-22 14:54:01 -0500

    homepage link

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home-full');
+  res.render('homepage');
 });
 
 module.exports = router;
